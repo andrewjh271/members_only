bde69f70396bf56822d18fda910e816a4e5dce19
Andrew Hayhurst <hayhurst.andrew@gmail.com>
2020-10-08 23:18:10 -0700

    Minor adjustments to scaffolding and associations for User/Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :bigint           not null, primary key
+#  title      :string
+#  body       :text
+#  author_id  :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Post < ApplicationRecord
-  belongs_to :author
+  belongs_to :author,
+    class_name: :User
 end
